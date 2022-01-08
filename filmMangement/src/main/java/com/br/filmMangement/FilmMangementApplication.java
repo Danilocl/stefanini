00eaaed29377a103df38dceb8669bd2549a6dbe4
Danilo Cardoso <danilocalessa@gmail.com>
2022-01-08 19:21:25 -0300

    udpate

--- filmMangement/src/main/java/com/br/filmMangement/FilmMangementApplication.java
+++ filmMangement/src/main/java/com/br/filmMangement/FilmMangementApplication.java
@@ -2,11 +2,12 @@ package com.br.filmMangement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @OpenAPIDefinition(info = @Info(title = "Films API", version = "1.0", description = "Films management API"))
 public class FilmMangementApplication {
 
